5277775e0143548836e608e501d4aa7791032fc2
nicolascavaille <nicolascavaille@172.16.3.77>
2017-09-21 23:03:04 +0200

    Patch App

--- src/fr/pizzeria/console/PizzeriaAdminConsoleApp.java
+++ src/fr/pizzeria/console/PizzeriaAdminConsoleApp.java
@@ -12,7 +12,7 @@ public class PizzeriaAdminConsoleApp {
 
 	public void execute() {
 		Scanner clavier = new Scanner(System.in);
-		Pizza[] pizzas = this.createPizzas();
+		Pizza[] pizzas = this.initPizzas();
 
 		int userInput = 0;
 
@@ -32,24 +32,30 @@ public class PizzeriaAdminConsoleApp {
 					String nom = clavier.next();
 					System.out.println("Veuillez saisir le prix");
 					double prix = clavier.nextDouble();
-					int id = pizzas.length + 1;
-					pizzas[id] = new Pizza(id, code, nom, prix);
-					//this.interfacePizza();
+					this.addPizza(pizzas, code, nom, prix);
 					break;
 				case 3:
 					System.out.println("Mise à jour d’une pizza\n");
 					System.out.println(allPizzas(pizzas));
 					System.out.println("Veuillez saisir le code de la pizza à modifier :\n");
 					System.out.println("(99 pour abandonner)\n");
-					String delCode = clavier.next();
-					deletePizza(delCode, pizzas);
-					//interfacePizza();
+					String modCode = clavier.next();
+					System.out.println("Veuillez saisir le nom (sans espace) :\n");
+					String modNom = clavier.next();
+					System.out.println("Veuillez saisir le prix");
+					double modPrix = clavier.nextDouble();
+					this.modifPizza(pizzas, modCode, modNom, modPrix);
 					break;
 				case 4:
 					System.out.println("Suppression d’une pizza\n");
+					System.out.println(allPizzas(pizzas));
+					System.out.println("Veuillez saisir le code de la pizza à supprimer :\n");
+					System.out.println("(99 pour abandonner)\n");
+					String delCode = clavier.next();
+					this.deletePizza(pizzas, delCode);
 					break;
 				case 99:
-					System.out.println("Au revoir \u1f60");
+					System.out.println("Au revoir!");
 					break;
 			}
 		} while (userInput != 99);
@@ -58,12 +64,15 @@ public class PizzeriaAdminConsoleApp {
 	}
 
 	public void interfacePizza() {
-		System.out.println("***** Pizzeria Administration *****\r\n" + "1. Lister les pizzas\r\n"
-				+ "2. Ajouter une nouvelle pizza\r\n" + "3. Mettre à jour une pizza\r\n" + "4. Supprimer une pizza\r\n"
+		System.out.println("***** Pizzeria Administration *****\r\n" 
+				+ "1. Lister les pizzas\r\n"
+				+ "2. Ajouter une nouvelle pizza\r\n" 
+				+ "3. Mettre à jour une pizza\r\n"
+				+ "4. Supprimer une pizza\r\n"
 				+ "99. Sortir");
 	}
 
-	public Pizza[] createPizzas() {
+	public Pizza[] initPizzas() {
 		Pizza[] pizzas = new Pizza[50];
 
 		pizzas[0] = new Pizza(0, "PEP", "Pépéroni", 12.50);
@@ -89,8 +98,27 @@ public class PizzeriaAdminConsoleApp {
 
 		return affich;
 	}
+	
+	public void addPizza(Pizza[] pizzas, String code, String nom, double prix) {
+		boolean added = false;
+		for (int i = 0; i < pizzas.length; i++) {
+			if (pizzas[i] == null && added == false) {
+				pizzas[i] = new Pizza(i, code, nom, prix);
+				added = true;
+			}
+		}
+	}
+	
+	public void modifPizza(Pizza[] pizzas, String code, String nom, double prix) {
+		for (int i = 0; i < pizzas.length; i++) {
+			if (pizzas[i] != null && pizzas[i].getCode().equals(code)) {
+				pizzas[i] = null;
+				pizzas[i] = new Pizza(i, code, nom, prix);
+			}
+		}
+	}
 
-	public void deletePizza(String code, Pizza[] pizzas) {
+	public void deletePizza(Pizza[] pizzas, String code) {
 		for (int i = 0; i < pizzas.length; i++) {
 			if (pizzas[i] != null && pizzas[i].getCode().equals(code)) {
 				pizzas[i] = null;
